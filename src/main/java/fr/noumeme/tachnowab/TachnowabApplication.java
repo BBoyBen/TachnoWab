01e905b85df1a6c9f5122b08693530f55f334983
Benoit <ben63112@gmail.com>
2020-03-11 13:51:34 +0100

    Ini projet spring

---
+++ src/main/java/fr/noumeme/tachnowab/TachnowabApplication.java
@@ -0,0 +1,13 @@
+package fr.noumeme.tachnowab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachnowabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TachnowabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/noumeme/tachnowab/TachnowabApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.noumeme.tachnowab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachnowabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
